8d72552fa2ba8e9eb3a6656ba49b1faea1be6efa
pesi1874 <pesi1874@gmail.com>
2020-08-17 22:12:26 +0800

    update img

--- split_img/main.py
+++ split_img/main.py
@@ -7,8 +7,12 @@ raw_path = '/data/grade2/wallpaper/imgs_3_31544'
 files_raw = os.listdir(raw_path)
 files_split = []
 for i in files_raw:
-    file = (i, i.split('.')[0].split('_')[1].split('x')[0], i.split('.')[0].split('_')[1].split('x')[1])
+    file = (i, int(i.split('.')[0].split('_')[1].split('x')[0]),
+            int(i.split('.')[0].split('_')[1].split('x')[1]))
     files_split.append(file)
+
+
+# ('p48205_1600x1200.jpg', '1600', '1200')
 
 
 def get_2000():
@@ -27,19 +31,48 @@ def get_2000():
         shutil.copyfile(old_path, new_path)
 
 
-def get_16_9():
-    path = os.path.join('/data/grade2/wallpaper', 'imgs_3_16_9')
+def get_4_3():
+    path = os.path.join('/data/grade2/wallpaper', 'imgs_3_4_3')
 
     if os.path.exists(path):
         shutil.rmtree(path)
     else:
         os.mkdir(path)
-    files = [i for i in files_split if int(i)>= 2560 and [1] >= 1.77 <= int(i[1]) / int(i[2]) <= 2]
-    print('16:9 : ', files.__len__())
+    files = [i for i in files_split if (i[2] >= 1440 and i[1] >= 2560 and 1.33 <= i[1] / i[2] < 2)]
+    print('4:3 : ', files.__len__())
+    copy_file(files, path)
+
+
+def get_6_3():
+    path = os.path.join('/data/grade2/wallpaper', 'imgs_3_6_3')
+
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    else:
+        os.mkdir(path)
+    files = [i for i in files_split if (i[2] >= 1440 and i[1] >= 2560 and 2 <= i[1] / i[2] < 2.66)]
+    print('6:3 : ', files.__len__())
+    copy_file(files, path)
+
+
+def get_8_3():
+    path = os.path.join('/data/grade2/wallpaper', 'imgs_3_8_3')
+
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    else:
+        os.mkdir(path)
+    files = [i for i in files_split if (i[2] >= 1440 and i[1] >= 2560 and i[1] / i[2] >= 2.66)]
+    print('8:3 : ', files.__len__())
+    copy_file(files, path)
+
+
+def copy_file(files, path):
     for i in files:
         old_path = os.path.join(raw_path, i[0])
         new_path = os.path.join(path, i[0])
         shutil.copyfile(old_path, new_path)
 
-# get_16_9()
-get_2000()
+# get_4_3()
+get_6_3()
+# get_8_3()
--- test.py
+++ test.py
@@ -38,3 +38,5 @@
 #
 # for i in range(5):
 #     print(i)
+
+
